9ddf132470c364b35be115093de4ab4bcfacd1bb
Raymond Brien <raytbrien@gmail.com>
2024-07-05 14:26:13 +0000

    Add initial quiz models

--- quiz/models.py
+++ quiz/models.py
@@ -2,12 +2,46 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
 
-# class Quiz(models.Model):
-#     """Stores a single quiz"""
-#     title = models.CharField(max_length=200, unique=True)
-#     description = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     image = CloudinaryField('image', default='placeholder') 
+class Quiz(models.Model):
+    """Stores a single quiz, which is a collection of questions"""
+
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    image = CloudinaryField('image', default='placeholder') 
+
+    class Meta:
+        ordering = ["-created_on"]
+    class __str__(self):
+        return f'Title: {self.title}'
+
+
+class Question(models.Model):
+    """Stores a single question related :model:`quiz.Quiz`"""
+
+    quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')
+
+    # TODO: check how to best format question text so it's multiple choice, see django docs
+    # TODO: question_options class? Or pulled from answers, with correct/incorrect bool on answers model? # changed from ERD question_text as each question is multiple choice 
+    question_text = models.TextField()
+    
+    # TODO: check how many types of point questions there are, and update integer choices accordingly. 
+    # https://docs.djangoproject.com/en/4.2/ref/models/fields/
+    class Points(models.IntegerChoices):
+        SMALL = 1
+        MEDIUM = 2
+        BIG = 3
+
+    points = models.IntegerField(choices=Points.choices)
+
+    # class Meta:
+
+
+class Answer(models.Model):
+    """Stores a single answer related :model:`quiz.Qusetion`"""
+    
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    answer_num = models.IntegerField()
+    correct = models.BooleanField() # additional field added after ERD
