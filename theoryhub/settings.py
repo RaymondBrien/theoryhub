26a5992f0a3dca1d106e6720b10cac76c6a63b1d
Raymond Brien <raytbrien@gmail.com>
2024-07-05 14:25:57 +0000

    Set up environment variables for security

--- theoryhub/settings.py
+++ theoryhub/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%9k74nk!iygem9e+lu$&cimtx6-4xo$1ggt-_=+-d0dq6(t##6'
+SECRET_KEY = os.environ.get("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
