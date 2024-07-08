6b1cdd4cb5bebd5170b39ce1d4b68811057e8719
Raymond <raytbrien@gmail.com>
2024-07-08 12:55:52 +0100

    Add quiz url to urlpatterns for access

--- theoryhub/urls.py
+++ theoryhub/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin 
+from django.urls import path, include
+from quiz import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', include('quiz.urls')),  # Include quiz URLs from quiz app
+    path('quiz/', views.QuizList.as_view(), name='quiz-urls'),
 ]
