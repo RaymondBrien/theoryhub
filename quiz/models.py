c3621eeaab940425a5fe475911a10bdff50a8932
Raymond <raytbrien@gmail.com>
2024-07-09 16:27:10 +0100

    Refactor and code-tidy with constants for easier handling

--- quiz/models.py
+++ quiz/models.py
@@ -4,23 +4,24 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator
 from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
+CORRECT = ((0, "Incorrect"), (1, "Correct" ))
+
+POINTS= ((1, "Small"), (2, "Medium" ))
+QUESTION_NUMBER = ((1, ""), (2, ""))
+ANSWER_OPTIONS = ((1, "A"), (2, "B"), (3, "C"), (4, "D"))
+
 class Quiz(models.Model):
     """Stores a single quiz, which is a collection of questions"""
 
-    title = models.CharField(max_length=200, unique=True, validators=[MinLengthValidator(5), MaxLengthValidator(20)] )
-    description = models.TextField(validators=[MinLengthValidator(5), MaxLengthValidator(50)] )
+    title = models.CharField(max_length=200, unique=True, validators=[MinLengthValidator(5), MaxLengthValidator(20)])
+    description = models.TextField(validators=[MinLengthValidator(5), MaxLengthValidator(50)])
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     image = CloudinaryField('image', default='placeholder')
     status = models.IntegerField(choices=STATUS, default =0)
 
-# TODO add messages if not valid field before try to save?
 # TODO Date field validation for published vs draft quiz - copy to questions too
-# https://docs.djangoproject.com/en/4.2/ref/models/instances/#django.db.models.Model.clean_fields
-    # STATUS_CHOICES = (
-    #     ('draft', 'Draft'),
-    #     ('published', 'Published'),
-    # )
+# https://docs.djangoproject.com/en/4.2/ref/models/instances/#django.db.models.Model.clean_field
     # def clean(self):
     # # Don't allow draft entries to have a pub_date.
     # if self.status == "draft" and self.pub_date is not None:
@@ -30,35 +31,55 @@ class Quiz(models.Model):
     #     self.pub_date = datetime.date.today()
     class Meta:
         ordering = ["-created_on"]
-        verbose_name_plural = 'Quizzes'
+        verbose_name_plural = "Quizzes"
     def __str__(self):
-        return f'Title: {self.title}'
+        return f'{self.title}'
 
 
 class Question(models.Model):
     """Stores a single question related :model:`quiz.Quiz`"""
-
-    quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')
-
+    # TODO: check how many types of point questions there are, and update integer choices accordingly. 
     # TODO: check how to best format question text so it's multiple choice, see django docs
     # TODO: question_options class? Or pulled from answers, with correct/incorrect bool on answers model? # changed from ERD question_text as each question is multiple choice 
-    question_text = models.TextField()
-    
-    # TODO: check how many types of point questions there are, and update integer choices accordingly. 
     # https://docs.djangoproject.com/en/4.2/ref/models/fields/
-    class Points(models.IntegerChoices):
-        SMALL = 1
-        MEDIUM = 2
-        BIG = 3
 
-    points = models.IntegerField(choices=Points.choices)
+    quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')
+    question_text = models.TextField()
+    question_image = CloudinaryField('image', default='placeholder')
+    points = models.IntegerField(choices=POINTS, default=1)
+        
+    class Meta:
+        ordering = ["question_text"]
+        verbose_name_plural = "Questions"
+    def __str__(self):
+        return f'Question: {self.question_text} (belongs to Quiz {self.quiz_id})'
+    
+#     QUESTIONS = {
+#     0: "",
+#     1: "What is your name?",
+#     2: "How old are you?",
+#     # ... more questions ...
+# }
+# from django.utils.translation import gettext_lazy as _
 
-    # class Meta:
+# class QuestionNumber(models.TextChoices):
+#     EMPTY = '', _('')
+#     QUESTION_1 = '1', _('What is your name?')
+#     QUESTION_2 = '2', _('How old are you?')
+#     # ... more questions ...
+
+#     question_number = models.IntegerField(choices=[(k, v) for k, v in QUESTIONS.items()])
+
 
 
 class Answer(models.Model):
     """Stores a single answer related :model:`quiz.Question`"""
     
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
-    answer_num = models.IntegerField()
-    correct = models.BooleanField() # additional field added after ERD
+    answer_option = models.IntegerField(choices=ANSWER_OPTIONS, default=1)
+    correct = models.IntegerField(choices=CORRECT, default=0) # additional field added after ERD
+    
+    class Meta:
+        ordering = ["answer_option"]
+        verbose_name_plural = "Answers"
+        unique_together = ("question_id", "answer_option") # only one answer per answer option per question
