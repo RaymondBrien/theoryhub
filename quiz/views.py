739289fcd953e94d394eb13195d62ad6b2c8adb6
Raymond <raytbrien@gmail.com>
2024-07-09 16:27:48 +0100

    Add context object name for template functionality

--- quiz/views.py
+++ quiz/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib import messages
 from .models import Quiz, Question, Answer
 
 
-class QuizList(generic.ListView):
+class Quizzes(generic.ListView):
     """
-    Displays a list of all quizzes in :model:`quiz.Quiz`
+    Displays a list of all quizzes in :model:`quiz.Quizzes`
     paginated by 6 quizzes per page.
     """
-    queryset = Quiz.objects.all()
+    queryset = Quiz.objects.filter(status=1) #  only display published quizzes
     template_name = 'quiz/quiz_list.html'
     paginate_by = 6
+    context_object_name = 'quizzes'
+
+
 
