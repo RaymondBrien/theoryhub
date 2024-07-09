2b69ea060d063e083e885f9c6d9e01953a1b52e8
Raymond <raytbrien@gmail.com>
2024-07-09 13:31:17 +0100

    Reformat Quiz admin registration for functionality

--- quiz/admin.py
+++ quiz/admin.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from .models import Quiz
 from django_summernote.admin import SummernoteModelAdmin
 
-@admin.register(Quiz)
-class QuizAdmin(SummernoteModelAdmin):
-    """
-    Admin for Quiz creation interface: 
-    create single instance of Quiz.
-    """
-    list_display = ('title', 'description', 'created_on', 'updated_on')
-    list_filter = ('created_on', 'updated_on')
-    search_fields = ('title', 'description')
-    ordering = ('-created_on',)
-    summernote_fields = ('description',)
+# @admin.register(Quiz)
+# class QuizAdmin(SummernoteModelAdmin):
+#     """
+#     Admin for Quiz creation interface: 
+#     create single instance of Quiz.
+#     """
+#     list_display = ('title', 'description', 'created_on', 'updated_on')
+#     list_filter = ('created_on', 'updated_on')
+#     search_fields = ('title', 'description')
+#     ordering = ('-created_on',)
+#     summernote_fields = ('description',)
 
-# admin.site.register(Quiz)
+admin.site.register(Quiz)
