b701f35e087e799dc7b21f326570f9a96b0b6089
Raymond <raytbrien@gmail.com>
2024-07-15 10:52:55 +0100

    Add dashboard app for UX development

---
+++ dashboard/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # path('', views.about.as_view(), name='about'), # TODO make view
+    # path('create-quiz/', views.CreateQuiz, name='create-quiz'),  # Add this path to create a new quiz instance
+]
