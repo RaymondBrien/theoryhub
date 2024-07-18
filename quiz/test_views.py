c45a6d00ba10c47009fed9b2694b2d951adaaf20
Raymond <raytbrien@gmail.com>
2024-07-18 11:19:28 +0100

    Set debug to false for deployment

--- quiz/test_views.py
+++ quiz/test_views.py
@@ -62,3 +62,76 @@ class TestQuizViews(TestCase):
     
     # def test_render_single_quiz_page(self):
     # def test_successful_answer_submission(self):
+    
+
+from django.test import TestCase
+from django.urls import reverse
+from quiz.models import Quiz, Question
+
+class SingleQuizViewTestCase(TestCase):
+
+    def setUp(self):
+        # Create a test quiz and questions
+        self.test_quiz = Quiz.objects.create(title='Test Quiz', slug='test-quiz', status=1)
+        self.question1 = Question.objects.create(question_text='Question 1', quiz_id=self.test_quiz)
+        self.question2 = Question.objects.create(question_text='Question 2', quiz_id=self.test_quiz)
+        self.question3 = Question.objects.create(question_text='Question 3', quiz_id=self.test_quiz)
+
+    def test_single_quiz_view_correct_questions(self):
+        # Get the URL for the single quiz view
+        url = reverse('single_quiz', args=[self.test_quiz.slug])
+
+        # Make a GET request to the URL
+        response = self.client.get(url)
+
+        # Check that the response status code is 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the correct number of questions are in the response
+        self.assertEqual(len(response.context['questions']), 3)
+
+        # Check that the questions in the response are the ones associated with the test quiz
+        for question in response.context['questions']:
+            self.assertEqual(question.quiz_id, self.test_quiz)
+
+    def test_single_quiz_view_no_questions(self):
+        # Create a test quiz with no questions
+        no_questions_quiz = Quiz.objects.create(title='No Questions Quiz', slug='no-questions-quiz', status=1)
+
+        # Get the URL for the single quiz view
+        url = reverse('single_quiz', args=[no_questions_quiz.slug])
+
+        # Make a GET request to the URL
+        response = self.client.get(url)
+
+        # Check that the response status code is 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the context 'questions' is empty
+        self.assertEqual(len(response.context['questions']), 0)
+
+    def test_single_quiz_view_invalid_slug(self):
+        # Create a test quiz
+        invalid_slug_quiz = Quiz.objects.create(title='Invalid Slug Quiz', slug='invalid-slug-quiz', status=1)
+
+        # Get the URL for the single quiz view with an invalid slug
+        url = reverse('single_quiz', args=['invalid-slug'])
+
+        # Make a GET request to the URL
+        response = self.client.get(url)
+
+        # Check that the response status code is 404 (Not Found)
+        self.assertEqual(response.status_code, 404)
+
+    def test_single_quiz_view_unpublished_quiz(self):
+        # Create a test quiz with status 0 (unpublished)
+        unpublished_quiz = Quiz.objects.create(title='Unpublished Quiz', slug='unpublished-quiz', status=0)
+
+        # Get the URL for the single quiz view
+        url = reverse('single_quiz', args=[unpublished_quiz.slug])
+
+        # Make a GET request to the URL
+        response = self.client.get(url)
+
+        # Check that the response status code is 404 (Not Found)
+        self.assertEqual(response.status_code, 404)
--- theoryhub/settings.py
+++ theoryhub/settings.py
@@ -28,7 +28,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = [
     'https://theoryhub-253c97b41326.herokuapp.com'
