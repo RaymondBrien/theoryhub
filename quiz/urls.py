318944c5911b534bdf97face2e031ba8606cd503
Raymond <raytbrien@gmail.com>
2024-07-23 16:29:46 +0100

    Add quiz result url to urls.py

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('quiz_list/', views.QuizList.as_view(), name='quiz_list'), # class based view
     path('<slug:slug>/', views.single_quiz, name='single_quiz'), # function based view
+    path('<slug:slug>/quiz_result/', views.quiz_result, name='quiz_result'), # function based view
+    
 ]
