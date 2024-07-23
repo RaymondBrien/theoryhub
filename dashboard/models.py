0ec78956d46f6db23f076ad1cc7c3f52e7ca9021
Raymond <raytbrien@gmail.com>
2024-07-23 12:17:11 +0100

    Add UserQuizScore model for user score functionality

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from quiz.models import Quiz
+
 
 # Create your models here.
+#  who owns 
+#  date 
+#  content 
+#  what quiz (imported quiz model) fk to quiz id 
+
+ 
+class UserQuizScore(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='user_quiz_score')
+    user_score = models.IntegerField(null=True)  # empty if no quiz taken yet
+    last_taken = models.DateField(auto_now_add=True)
+ 
