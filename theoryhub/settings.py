9d70b20cb0f58207ef2a79f5ced731c2456426f0
Raymond <raytbrien@gmail.com>
2024-07-25 13:53:57 +0100

    Debug to enable pytest to use test db

--- theoryhub/settings.py
+++ theoryhub/settings.py
@@ -116,7 +116,12 @@ DATABASES = {
 
 if 'test' in sys.argv:
     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
-    
+
+if 'pytest' in sys.modules:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
     
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
