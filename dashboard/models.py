3d598619b21539e5166176a21ab29235f052afd7
Raymond <raytbrien@gmail.com>
2024-07-25 14:08:31 +0100

    Add QuizNote model to dashboard models

--- dashboard/models.py
+++ dashboard/models.py
@@ -7,4 +7,13 @@ class UserQuizSubmission(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='user_quiz_score')
     user_score = models.IntegerField(null=True, default=0)  # empty if no quiz taken yet
     last_taken = models.DateField(auto_now_add=True)
- 
+
+
+class QuizNote(models.Model):
+    """
+    Stores single note related to :model:`quiz.Quiz` and :model:`auth.User`
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='quiz_notes')
+    note = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
