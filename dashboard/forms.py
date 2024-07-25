a6668be5268668fba113aa91debec68818df549d
Raymond <raytbrien@gmail.com>
2024-07-25 14:56:06 +0100

    Add quiz_note form template to dashboard app

---
+++ dashboard/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import QuizNote
+
+
+class QuizNoteForm(forms.ModelForm):
+    class Meta:
+        model = QuizNote
+        fields = ['note', ]
+        widgets = {
+            'note': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter your note here'})
+        }
