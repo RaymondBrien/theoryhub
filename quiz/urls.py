19ad71f3c7a60495bb5aab8bea5f5f3da6f33f67
Raymond <raytbrien@gmail.com>
2024-08-01 16:37:56 +0100

    Code tidy

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,8 +4,5 @@ from . import views
 urlpatterns = [
     path('quiz_list/', views.QuizList.as_view(), name='quiz_list'),
     path('<slug:slug>/', views.single_quiz, name='single_quiz'),
-    # TODO add quiz submit view for url below
-    # path('<slug:slug>/submit_quiz/<int:user_id>', views.submit_quiz, name='submit_quiz'),
     path('<slug:slug>/quiz_result/', views.quiz_result, name='quiz_result'),
-    
 ]
