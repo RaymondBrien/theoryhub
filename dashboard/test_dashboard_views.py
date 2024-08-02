995951cd52322173639c4a20435617b0f833bf92
Raymond <raytbrien@gmail.com>
2024-08-02 12:13:38 +0100

    Reformat dashboard testing for clearer debugging strategy

--- dashboard/test_dashboard_views.py
+++ dashboard/test_dashboard_views.py
@@ -1,13 +1,16 @@
 import pytest
-from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.auth.models import User
 from django.test import TestCase, Client, RequestFactory
 from django.contrib.auth.models import User
 from django.urls import reverse, resolve
 
+from .models import QuizNote
+from .forms import QuizNoteForm
 
-# TODO dashboard(function)
-# TODO delete note (function)
+
+# TODO user creates note
 # TODO edit note (function)
+# TODO delete note (function)
 # TODO UserNote (class view)
 
 
@@ -20,7 +23,31 @@ class TestDashboardViews(TestCase):
         response = self.client.get(reverse('dashboard'))
         self.assertEqual(response.status_code, 200)
     
-    def test_dashboard_view_returns_404_if_user_not_authenticated(self):
+    def test_dashboard_view_redirects_to_login_page_if_user_not_authenticated(self):
         self.client.logout()
-        response = self.client.get(reverse('dashboard')) # TODO add user auth validation in view handling so can't be brute forced
-        self.assertEqual(response.status_code, 404)
+        response = self.client.get(reverse('dashboard'))
+        self.assertRedirects(response, '/accounts/login/?next=/dashboard/')
+
+    
+class TestQuizNotes(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+        self.quiznote = QuizNote.objects.create(user=self.user, note='Test note')
+        self.quiznote.save()
+    
+    def test_render_notes_page_includes_quiz_note_form(self):
+        """Test when loading notes page that the user can make a
+        post request to post QuizNote instance"""
+        response = self.client.get(reverse('user_notes'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context['quiz_note_form'], QuizNoteForm)
+        
+    def test_user_note_submission(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post(reverse('user_notes'), {'note': 'This is a test note'})
+        self.assertEqual(response.status_code, 200)
+        in
+        # self.assertTrue(QuizNote.objects.filter(note='This is a test note').exists())
+        self.assertIn('Quiznote added', response.content)
