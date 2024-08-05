321855cd16c9e7fb858309ef8b8fbb255005c907
Raymond <raytbrien@gmail.com>
2024-08-05 18:30:12 +0100

    Set debug to false for deployment

--- theoryhub/settings.py
+++ theoryhub/settings.py
@@ -31,7 +31,7 @@ STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 SECRET_KEY = os.environ.get("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = [
     'https://theoryhub-253c97b41326.herokuapp.com'
